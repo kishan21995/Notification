64adc4e1b39f19fddb3e1cb893c72f03e35f7cb3
Kishan <kishangupta21995@gmail.com>
2019-11-12 14:56:04 -0500

    Initial commit

---
+++ app/src/main/java/com/app/notification/MyMessagingService.java
@@ -0,0 +1,32 @@
+package com.app.notification;
+
+import android.app.NotificationManager;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String title,String message){
+
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"MyNotifications")
+        .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message);
+
+        NotificationManagerCompat managerCompat=NotificationManagerCompat.from(this);
+        managerCompat.notify(999,builder.build());
+
+    }
+
+}
